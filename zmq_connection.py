79e9512264495269acda92dd84a1aa05a2b1cee0
Ken <kennethdesrosiers@gmail.com>
2020-12-06 12:57:33 -0500

    done for now, finished connection status socket

--- zmq_connection.py
+++ zmq_connection.py
@@ -1,6 +1,4 @@
 import socket
-import sys
-import time
 import zmq
 import base64
 import cv2
@@ -11,8 +9,10 @@ class PiZMQMessager:
         self.context = zmq.Context()
         self.stream_socket = self.context.socket(zmq.REP)
         self.sound_socket = self.context.socket(zmq.REP)
+        self.connection_socket = self.context.socket(zmq.PUB)
         self.stream_socket.bind("tcp://*:5555")
         self.sound_socket.bind("tcp://*:5556")
+        self.connection_socket.bind("tcp://*:5557")
         self.video = cv2.VideoCapture("rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov")
         while True:
             try:
@@ -21,7 +21,7 @@ class PiZMQMessager:
                 self.audio(sound_message)
             except:
                 pass
-
+            self.connection_socket.send_string("c")    
             stream_message = self.stream_socket.recv_string()
             grabbed, frame = self.video.read()
             encoded, buffer = cv2.imencode(".jpg", frame)
@@ -33,8 +33,6 @@ class PiZMQMessager:
         file = open("/home/pi/Desktop/test.wav","wb")
         file.write(wav)
         file.close()
+        
 
 test = PiZMQMessager()
-
-#sudo netstat -ltnp
-#kill -9 pid
