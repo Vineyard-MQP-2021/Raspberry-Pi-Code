254f812bacb1d9c05f9f2a551832c9dc963102d2
Ken <kennethdesrosiers@gmail.com>
2020-12-08 12:50:13 -0500

    refactored code for object diagram

--- zmq_connection.py
+++ zmq_connection.py
@@ -15,24 +15,30 @@ class PiZMQMessager:
         self.connection_socket.bind("tcp://*:5557")
         self.video = cv2.VideoCapture("rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov")
         while True:
-            try:
-                sound_message = self.sound_socket.recv(zmq.NOBLOCK)
-                self.sound_socket.send_string("the pi got the file!")
-                self.audio(sound_message)
-            except:
-                pass
-            self.connection_socket.send_string("c")    
-            stream_message = self.stream_socket.recv_string()
-            grabbed, frame = self.video.read()
-            encoded, buffer = cv2.imencode(".jpg", frame)
-            frame = base64.b64encode(buffer)
-            self.stream_socket.send(frame)
+            self.saveAudio()
+            self.sendConnectionStatus()
+            self.sendStream()
 
-    def audio(self, sound):
-        wav = base64.b64decode(sound)
-        file = open("/home/pi/Desktop/test.wav","wb")
-        file.write(wav)
-        file.close()
+    def saveAudio(self):
+        try:
+            sound = self.sound_socket.recv(zmq.NOBLOCK)
+            self.sound_socket.send_string("the pi got the file!")
+            wav = base64.b64decode(sound)
+            file = open("/home/pi/Desktop/test.wav","wb")
+            file.write(wav)
+            file.close()
+        except:
+            pass
+
+    def sendConnectionStatus(self):
+        self.connection_socket.send_string("c")
+
+    def sendStream(self):
+        stream_message = self.stream_socket.recv_string()
+        grabbed, frame = self.video.read()
+        encoded, buffer = cv2.imencode(".jpg", frame)
+        frame = base64.b64encode(buffer)
+        self.stream_socket.send(frame)
+
         
-
 test = PiZMQMessager()
